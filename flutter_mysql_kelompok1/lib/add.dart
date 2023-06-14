81f08e590dfc40b621a99914ea52ef230ff9e6d3
palcomtech <elliciaserliyaw@gmail.com>
2023-06-14 23:38:55 +0700

    add

---
+++ flutter_mysql_kelompok1/lib/add.dart
@@ -0,0 +1,146 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
+import 'package:flutter_mysql/models/note_provider.dart';
+
+class Add extends StatefulWidget {
+  const Add({super.key});
+
+  @override
+  State<Add> createState() => _AddState();
+}
+
+class _AddState extends State<Add> {
+  final _formKey = GlobalKey<FormState>();
+
+  var title = TextEditingController();
+  var content = TextEditingController();
+
+  Future _onSubmit() async {
+    try {
+      final response = await http.post(
+        Uri.parse("http://192.168.1.7/note_app/create.php"),
+        body: {
+          "title": title.text,
+          "content": content.text,
+        },
+      );
+
+      if (response.statusCode == 200) {
+        var data = jsonDecode(response.body);
+        print(data["message"]);
+
+        Navigator.pop(context, {
+          "title": title.text,
+          "content": content.text,
+        });
+      } else {
+        throw Exception('Failed to create note');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Create New Note"),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Container(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Title',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 5),
+              TextFormField(
+                controller: title,
+                decoration: InputDecoration(
+                  hintText: "Type Note Title",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Note Title is Required';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Content',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 5),
+              TextFormField(
+                controller: content,
+                keyboardType: TextInputType.multiline,
+                minLines: 5,
+                maxLines: null,
+                decoration: InputDecoration(
+                  hintText: "Type Note Content",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Note Content is Required';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 15),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+                child: const Text(
+                  "Submit",
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _onSubmit();
+                  }
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
