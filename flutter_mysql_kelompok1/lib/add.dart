983a3b3132126237534605c8a9333d22e7cec8e5
asraf76 <gustomi18012000@gmail.com>
2023-06-18 19:08:25 +0700

    models

--- flutter_mysql_kelompok1/lib/add.dart
+++ flutter_mysql_kelompok1/lib/add.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
-import 'package:flutter_mysql/models/note_provider.dart';
+import 'package:flutter_mysql_kelompok1/models/note.dart';
 
 class Add extends StatefulWidget {
   const Add({super.key});
